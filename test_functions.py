88ebc309951fa73735c600434af0fa2f2f1c141a
Craig Richards <craig@greywidget.co.uk>
2023-05-30 21:11:45 +0100

    Introduce failing test

--- test_functions.py
+++ test_functions.py
@@ -9,8 +9,9 @@ def test_add():
 
 
 # uncomment the following test in step 5
-# def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
+
 
 # uncomment the following test in step 11
 # def test_convert_fahrenheit_to_celsius():
